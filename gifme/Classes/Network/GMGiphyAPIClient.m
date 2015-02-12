5fee7155a7314aeac5a33da068165d95f8619ded
Will Kalish <williamkalish@WilliamKalish-MBP.attlocal.net>
2015-02-12 12:09:28 -0600

    Added api client for requests to Giphy

---
+++ gifme/Classes/Network/GMGiphyAPIClient.m
@@ -0,0 +1,54 @@
+//
+//  GMGiphyAPIClient.m
+//  gifme
+//
+//  Created by William Kalish on 2/12/15.
+//  Copyright (c) 2015 William Kalish. All rights reserved.
+//
+
+#import "GMGiphyAPIClient.h"
+
+@implementation GMGiphyAPIClient
+
++ (instancetype)sharedClient {
+    static GMGiphyAPIClient *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[GMGiphyAPIClient alloc] init];
+    });
+    return _sharedManager;
+}
+
+- (void)fetchTrendingGifs:(void (^)(id json, NSError* error))completionHandler {
+    NSURL *url = [NSURL URLWithString:@"http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"];
+    [self fetchGifsWithURL:url completionHandler:completionHandler];
+}
+
+- (void)searchGif:(NSString *)searchTerm completionHandler:(void (^)(id json, NSError* error))completionHandler {
+    NSString *urlSafeSearchTerm = [searchTerm stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *urlString = [NSString stringWithFormat:@"http://api.giphy.com/v1/gifs/search?q=%@&api_key=dc6zaTOxFJmzC", urlSafeSearchTerm];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    [self fetchGifsWithURL:url completionHandler:completionHandler];
+}
+
+- (void)fetchGifsWithURL:(NSURL *)url completionHandler:(void (^)(id json, NSError* error))completionHandler {
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (completionHandler) {
+            if (error) {
+                completionHandler(nil, error);
+            } else {
+                NSError *jsonError = nil;
+                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+                completionHandler(json, jsonError);
+            }
+        }
+    }];
+    
+    [task resume];
+}
+
+@end
